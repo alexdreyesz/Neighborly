ccdce9b6a0429218025e8c6cc499d7c89b1d9b33
alexdreyesz <alexdreyesz@gmail.com>
2025-09-27 17:05:29 -0400

    Fixed Merge Conflicts

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,15 +1,5 @@
 export { default as Landing } from './Landing/Landing.tsx'
 export { default as Onboarding } from './Onboarding/Onboarding.tsx'
 export { default as UserProfile } from './UserProfile/UserProfile.tsx'
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-export { default as GeminiNeeds } from './GeminiNeeds/GeminiNeeds.tsx'
-=======
-export { Login, SignUp } from './Auth'
->>>>>>> origin/main
-=======
 export { default as GeminiNeeds } from './GeminiNeeds/GeminiNeeds.tsx'
 export { Login, SignUp } from './Auth'
-
->>>>>>> 9cf981994ce1958f2bc629ce6b44e88d042b2aeb
